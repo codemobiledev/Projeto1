b7a716322e50e101b5dcfb9db794454447c34430
Alisson <advieirabr@gmail.com>
2017-08-19 11:59:23 -0300

    Commit Inicial - 5 Activities criadas (avisos, cadastro, envio mensagem, inicial e usuário identificado)

---
+++ app/src/main/java/com/codemobiledev/projeto1/TelaEnvioMensagem.java
@@ -0,0 +1,13 @@
+package com.codemobiledev.projeto1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TelaEnvioMensagem extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_envio_mensagem);
+    }
+}
