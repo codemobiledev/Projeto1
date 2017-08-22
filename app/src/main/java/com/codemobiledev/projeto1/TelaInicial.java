924ebb55539b6b6cf2124ca518a7afb16827043b
x7137367 <tomazaugustomsn@hotmail.com>
2017-08-22 19:01:54 -0300

    Commit teste comentário adicionado.

--- app/src/main/java/com/codemobiledev/projeto1/TelaInicial.java
+++ app/src/main/java/com/codemobiledev/projeto1/TelaInicial.java
@@ -9,5 +9,6 @@ public class TelaInicial extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela_inicial);
+        //Teste Github
     }
 }
