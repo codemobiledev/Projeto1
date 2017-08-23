fec140b04f6dc411195a5221678eabc2a8378aa9
codemobiledev <31112729+codemobiledev@users.noreply.github.com>
2017-08-23 19:39:48 -0300

    Update TelaInicial.java

--- app/src/main/java/com/codemobiledev/projeto1/TelaInicial.java
+++ app/src/main/java/com/codemobiledev/projeto1/TelaInicial.java
@@ -9,6 +9,7 @@ public class TelaInicial extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela_inicial);
-
+//teste
+       
     }
 }
